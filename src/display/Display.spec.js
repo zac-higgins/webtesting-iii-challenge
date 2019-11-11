443a4be86672d02db8c9b67e5ae124f9261802b9
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-11-11 16:19:57 -0700

    added test to check gate defaults

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { render } from 'react-testing-library';
-
 import Display from './Display';
 
-test('it renders correctly', () => {
-    expect(render(<Display />)).toMatchSnapshot();
-});
+
+test('unlocked and open', () => {
+
+    expect(Display.defaultProps.closed).toBe(false)
+    expect(Display.defaultProps.locked).toBe(false)
+})
 
