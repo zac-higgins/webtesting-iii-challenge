cf98c3be35e54435c87ea9a91ca8650347f0b282
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-11-12 08:56:09 -0700

    added dashboard test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,7 +2,21 @@ import React from "react";
 import { render } from 'react-testing-library';
 
 import Controls from './Controls';
+import Dashboard from '../dashboard/Dashboard'
 
 test('it renders correctly', () => {
     expect(render(<Controls />)).toMatchSnapshot();
+});
+
+test('when closed and locked, open is disabled and unlock is enabled', () => {
+    const closedMock = jest.fn();
+    const lockedMock = jest.fn();
+
+    const { getByText } = render(<Controls closed={true} locked={true} toggledClosed={closedMock} toggledLocked={lockedMock} />);
+
+    const openButton = getByText(/open gate/i);
+    const unlockButton = getByText(/unlock gate/i);
+
+    expect(openButton.disabled).toBeTruthy();
+    expect(unlockButton.disabled).toBeFalsy();
 });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -6,3 +6,17 @@ import Dashboard from './Dashboard';
 test('it renders correctly', () => {
     expect(render(<Dashboard />)).toMatchSnapshot();
 });
+
+test('shows the controls and display', async () => {
+    const { findAllByText } = render(<Dashboard />)
+
+    const unlocked = await findAllByText(/unlocked/i);
+    const open = await findAllByText(/open/i);
+    const lock = await findAllByText(/lock/i);
+    const close = await findAllByText(/close/i);
+
+    expect(unlocked);
+    expect(open);
+    expect(lock);
+    expect(close);
+})
